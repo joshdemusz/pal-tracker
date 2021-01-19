c8b80996eb28b07a2df173cbd04e7bee2d75c4eb
Ubuntu <ubuntu@ip-172-31-82-232.ec2.internal>
2021-01-19 16:54:32 +0000

    Add rest of environment lab code

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public String message;
+
+    public WelcomeController(@Value("${welcome.message}") String m) {
+        this.message = m;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return this.message;
     }
 }
